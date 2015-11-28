ba83fd2224280afc43cc2eaa9836429751f03673
AltimorTASDK <altimormc@gmail.com>
2015-11-28 12:18:27 -0500

    reverted broken input filtering change, cleaned up psikyosh.c changes

--- src/emu/inptport.c
+++ src/emu/inptport.c
@@ -193,7 +193,6 @@ struct _digital_joystick_state
 	UINT8						current;			/* current value */
 	UINT8						current4way;		/* current 4-way value */
 	UINT8						previous;			/* previous value */
-	UINT8						previous4way;		/* previous 4-way value */
 	UINT8						current_from_seq;
 	UINT8						previous_from_seq;
 };
@@ -2613,6 +2612,8 @@ static void frame_update_digital_joysticks(running_machine *machine)
 				/* only update 4-way case if joystick has moved */
 				if (joystick->current != joystick->previous)
 				{
+					joystick->current4way = joystick->current;
+
 					/*
                         If joystick is pointing at a diagonal, acknowledge that the player moved
                         the joystick by favoring a direction change.  This minimizes frustration
@@ -2624,8 +2625,11 @@ static void frame_update_digital_joysticks(running_machine *machine)
 
                         Zero any switches that didn't change from the previous to current state.
                      */
-					joystick->previous4way = joystick->current4way;
-					joystick->current4way = joystick->current & ~joystick->previous4way;
+					if ((joystick->current4way & (JOYDIR_UP_BIT | JOYDIR_DOWN_BIT)) &&
+						(joystick->current4way & (JOYDIR_LEFT_BIT | JOYDIR_RIGHT_BIT)))
+					{
+						joystick->current4way ^= joystick->current4way & joystick->previous;
+					}
 
 					/*
                         If we are still pointing at a diagonal, we are in an indeterminant state.
@@ -2642,9 +2646,9 @@ static void frame_update_digital_joysticks(running_machine *machine)
 						(joystick->current4way & (JOYDIR_LEFT_BIT | JOYDIR_RIGHT_BIT)))
 					{
 						//if (mame_rand(machine) & 1)
-						//	joystick->current4way &= ~(JOYDIR_LEFT_BIT | JOYDIR_RIGHT_BIT);
+							joystick->current4way &= ~(JOYDIR_LEFT_BIT | JOYDIR_RIGHT_BIT);
 						//else
-							joystick->current4way &= ~(JOYDIR_UP_BIT | JOYDIR_DOWN_BIT);
+						//	joystick->current4way &= ~(JOYDIR_UP_BIT | JOYDIR_DOWN_BIT);
 					}
 				}
 			}
